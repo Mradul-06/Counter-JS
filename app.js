044af12b14f4a4c17c9323e31fe34d923154f0ab
Mradul Agrawal <mradulagrawal126@gmail.com>
2021-08-28 23:07:18 +0530

    intial

---
+++ app.js
@@ -0,0 +1,10 @@
+const decreasebtn = document.querySelector(".btndecrease")
+let resetbtn = document.querySelector(".bt reset")
+let increasebtn = document.querySelector(".btn increase")
+let value = document.querySelector("#value")
+
+function decreaseValue(){
+    console.log("hello")
+}
+
+decreasebtn.addEventListener("click",decreaseValue())
